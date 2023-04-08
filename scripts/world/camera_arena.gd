6724520d087c7f73963b4e9270514daed1771336
Matheus arruda <matthmsc@gmail.com>
2023-04-08 10:59:43 -0300

    camera ajustada ao painel

--- scripts/world/camera_arena.gd
+++ scripts/world/camera_arena.gd
@@ -3,15 +3,20 @@ class_name CameraArena extends Camera2D
 const POSITION_CENTER := Vector2(960, 540)
 const MIN_ZOOM := 0.8
 const MAX_ZOOM := 1.6
+const DISTANCE_FOCUS := 0.15
 
 @export var turbulence_force := 1.0
 
 @export_node_path("CanvasLayer") var background_path: NodePath
 @onready var background: CanvasLayer = get_node(background_path)
 
+@export var element_info_path: NodePath
+@onready var element_info: PanelContainer = get_node(element_info_path)
+
 var time := 0.0
 var extra_zoom := 1.0
 var turbulence: Vector2
+var distance_fucus: float
 var turbulence_target_0: Vector2
 var turbulence_target_1: Vector2
 
@@ -28,8 +33,15 @@ func _physics_process(delta):
 	
 	if Gameplay.selected_element:
 		if is_instance_valid(Gameplay.selected_element):
-			global_position = ((Gameplay.selected_element.position - POSITION_CENTER) * 0.15 * extra_zoom) + POSITION_CENTER + turbulence
+			distance_fucus = DISTANCE_FOCUS
+			
+			if element_info.visible:
+				distance_fucus *= 3.0
+			
+			global_position = ((Gameplay.selected_element.position - POSITION_CENTER) * distance_fucus * extra_zoom) + POSITION_CENTER + turbulence
+			
 		zoom = lerp(zoom, Vector2(extra_zoom, extra_zoom) * Vector2(1.25, 1.25), 0.2)
+		
 	else:
 		global_position = POSITION_CENTER + turbulence
 		zoom = lerp(zoom, Vector2(extra_zoom, extra_zoom), 0.1)
